b1a93eb59eefb432f9763c62943c4f6023ca2a8a
Michelle Totherow <tym4me13@gmail.com>
2024-01-23 11:16:59 -0600

    update to navbar import

--- src/PersonalPlanner.jsx
+++ src/PersonalPlanner.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { ApplicationViews } from './views/ApplicationViews'
-import {NavBar} from '/home/josh/workspace/personal_planner/PersonalPlannerClient/PersonalPlannerClient/src/components/nav/NavBar.jsx'
+import { NavBar } from "./components/nav/NavBar"
+
+
 
 export const PersonalPlanner = () => {
 
