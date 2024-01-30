fb124c11cfba415679902519c17485a41781cd5e
Daniel Bennett <danielpsalmbennett@yahoo.com>
2024-01-30 14:45:06 -0600

    added some work in progress login

--- src/views/Authorized.jsx
+++ src/views/Authorized.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom"
 
-export const Authorized = ({ token }) => {
-  if (token) {
+export const Authorized = ({ currentUser, setUser  }) => {
+  if (currentUser) {
     return <Outlet />
   }
   return <Navigate to='/login' replace />
